bd11e4f9d7a116814e5c33ce079257336228ac2d
saphst4r <saphst4r@naver.com>
2022-10-25 20:42:41 +0900

    add copy link functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,11 +6,13 @@ import { db } from '../firebase';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from 'swiper';
 import 'swiper/css/bundle'
+import {RiShareBoxLine} from 'react-icons/ri'
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkedCopied] = useState(false);
     SwiperCore.use([Autoplay, Navigation, Pagination]);
     useEffect(()=>{
         async function fetchListing(){
@@ -41,7 +43,7 @@ export default function Listing() {
                     <div 
                         className='relative w-full overflow-hidden h-[300px]' 
                         style={{
-                            background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                            background: `url(${listing.imgUrls[index]}) center no-repeat`, 
                             backgroundSize: 'cover',
                         }}>
 
@@ -49,6 +51,18 @@ export default function Listing() {
                 </SwiperSlide> 
             ))}
         </Swiper>
+        <div className='fixed top-[8%] right-[3%] z-10 cursor-pointer bg-white w-12 h-12 border-2 border-gray-400 rounded-lg flex justify-center items-center  opacity-50 shadow-md hover:shadow-lg hover:opacity-100 transition duration-150 ease-in-out' onClick={()=>{
+            navigator.clipboard.writeText(window.location.href)
+            setShareLinkedCopied(true)
+            setTimeout(()=>{
+                setShareLinkedCopied(false)
+            }, 2000);
+        }}>
+            <RiShareBoxLine className='text-3xl text-slate-500'/>
+        </div>
+        {shareLinkCopied && (
+            <p className='fixed top-[8%] right-[10%] font-semibold shadow-lg rounded-md bg-white z-10 p-2'>Link Copied</p>
+        )}
     </main>
   )
 }
